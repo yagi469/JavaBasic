558b19c8acf28791b7776130956af93ed26a870f
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2021-06-17 09:50:38 +0900

    Update UseArrayList.java

--- UseArrayList.java
+++ UseArrayList.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+class UseArrayList {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<String>();
+    list.add("Hello");
+    list.add("Java");
+    list.add("おはよう");
+    list.add("こんにちは");
+    for (int i=0; i<list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+    System.out.println("-------remove-------");
+    list.remove(2);
+    for (String str : list) {
+      System.out.println(str);
+    }         
+  }
+}
