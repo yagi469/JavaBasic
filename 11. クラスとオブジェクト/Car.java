188d4c923077a993cdb3eca5b1e7804d4aca79dd
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2021-06-20 07:22:19 +0900

    Add files via upload

---
+++ 11. クラスとオブジェクト/Car.java
@@ -0,0 +1,17 @@
+class Car {
+    String maker;        // メーカー
+    int displacement;    // 排気量
+    String color;        // 色
+
+    void start() {
+        System.out.println("発進します");
+    }
+
+    void stop() {
+        System.out.println("止まります");
+    }
+
+    void curve {
+        System.out.println("曲がります");
+    }
+}
